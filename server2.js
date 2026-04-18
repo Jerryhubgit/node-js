d38a9b9e23a0b5bca2f0f17c45d8038ed1659703
jerryhubgit <chimobijeremaih@gmail.com>
2026-04-18 20:43:34 +0100

    basic routing with http

--- server2.js
+++ server2.js
@@ -1,27 +1,19 @@
-import { createServer } from 'http'
+import  { createServer } from 'http'
 
-const users = [ 
-    { id: 1, name: 'Benjamin frankwell' }, 
-    { id: 2, name: 'phillip wells' }, 
-    { id: 3, name: 'thomans haggi' }
+
+const users = [
+    { id: 1, name: 'James ibori' }, 
+    { id: 2, name: 'Philip morgan' }, 
+    { id: 3, name: 'Benjamin franklin' }
 ]
 
 const server = createServer((req, res) => {
-    console.log(req.url === '/api/users')
-    console.log(req.method === 'GET')
-    if(req.url = '/api/users' && req.method === 'GET'){ 
-        res.setHeader('Content-Type', 'application/json' )
-        res.write(JSON.stringify(users))
-        res.end()
-    }else{
-        res.setHeader('Content-Type','application/json' )
-        res.statusCode = 404
-        res.write(JSON.stringify({ message: 'Route not found' }))
-        res.end()
-    }
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(users))
+    res.end()
 })
 
-const PORT = 2000 
+const PORT = 2000; 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server running on http://localhost:${PORT}`)
 })
--- ttp.js
+++ ttp.js
@@ -1,34 +1,41 @@
-import http from 'http'
-import path from 'path'
-import fs from 'fs/promises'
-import url from 'url'
+import { createServer } from 'http'
 
 
-const server = http.createServer(async (req, res) => {
-    const __filename =  url.fileURLToPath(import.meta.url)
-    const __dirname = path.dirname(__filename)
+const users = [
+    { id: 0,  name: 'James', color: 'red' }, 
+    { id: 1, name: 'Benjamin', color: 'green' }
+]
 
-    let filePath;
-    if(req.method === 'GET'){
-        if(req.url === '/'){
-            filePath = path.join(__dirname, 'public', 'index.html')
-        }else if(req.url === '/about'){
-            filePath = path.join(__dirname, 'public', 'static.html')
-        }else{
-            console.log('not a valid route')
+const server  = createServer((req, res) => {
+    try{
+        if(req.method === 'GET'){
+            if(req.url === '/about'){
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end('<h1> About page </h1>')
+            }else if(req.url.match(/\api\/users\/([0-9]+)/)){
+                let id = req.url.split('/')[3]
+                let user = users.find((user) => user.id === Number(id) )
+                
+                console.log(user)
+
+                if(user) { 
+                    res.writeHead(200, { 'Content-Type': 'application/json' })
+                    res.end(JSON.stringify(user))
+                }else res.end('User doesn\'t exist' )
+
+
+            }else{
+                res.writeHead(404, { 'Content-Type': 'text/plain' })
+                res.end('Not Found')
+            }
         }
 
-        let data = await fs.readFile(filePath)
-        res.end(data)
-    }else{
-        res.writeHead(500, { 'Content-Type': 'text/plain'})
-        res.setHeader({ 'Content-Type': 'text/html' })
-        res.end('Server Error')
+    }catch(err){
+        throw new Error('Server Error')
     }
-    
 })
 
-const PORT  = process.env.PORT
+const PORT = 3000
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
